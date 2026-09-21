fc9c0ab509972fb9bc5f152de499998980d45f27
mohsen <bis.mohsentabasi@gmail.com>
2026-09-21 22:25:20 +0100

    Updated_Copyright

--- Source/AssetAnalytics/Private/AssetAnalytics.cpp
+++ Source/AssetAnalytics/Private/AssetAnalytics.cpp
@@ -1,4 +1,5 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright (c) 2026 Mohsen Tabasi
+// SPDX-License-Identifier: MIT
 
 #include "AssetAnalytics.h"
 #include "Core/AssetAnalyticsReportBridge.h"
